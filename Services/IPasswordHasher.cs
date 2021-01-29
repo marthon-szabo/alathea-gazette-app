0cda3dde5b57707127c3a54283cb074ee616df8f
marthon-szabo <makotoimx@gmail.com>
2021-01-29 15:25:54 +0100

    Create IPasswordHasher interface

---
+++ Services/IPasswordHasher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlatheaGazette.Services
+{
+    public interface IPasswordHasher
+    {
+        public string[] HashMe(string plainText);
+        
+    }
+}
