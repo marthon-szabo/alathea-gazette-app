be82879903bafdfe54b6d967cac1c62766293e00
marthon-szabo <makotoimx@gmail.com>
2021-01-29 15:22:52 +0100

    Implement IUserfactory

--- Services/UserFactory.cs
+++ Services/UserFactory.cs
@@ -1,16 +1,26 @@
 using AlatheaGazette.Models;
 
+
 namespace AlatheaGazette.Services
 {
     public class UserFactory : IUserFactory
     {
-        public UserModel createInstance(int id, string email, string password)
+        private IPasswordHasher _passwordHasher;
+
+        public UserFactory(IPasswordHasher passwordHasher)
         {
+            _passwordHasher = passwordHasher;
+        }
+
+        public UserModel createInstance(string email, string password)
+        {
+            string[] saltAndPassword = _passwordHasher.HashMe(password);
+
             return new UserModel()
             {
-                UserId = id,
                 Email = email,
-                Password = password
+                PasswordSalt = saltAndPassword[0],
+                HashedPassword = saltAndPassword[1]
             };
         }
     }
