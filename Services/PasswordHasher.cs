9bc8b91113dce0407d01731477939c0e6809faa4
marthon-szabo <makotoimx@gmail.com>
2021-01-29 15:26:07 +0100

    Implement IPasswordHasher

---
+++ Services/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace AlatheaGazette.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string[] HashMe(string plainText)
+        {
+            StringBuilder saltedPassword = new StringBuilder();
+            string salt = BCrypt.Net.BCrypt.GenerateSalt();
+            saltedPassword.Append(salt);
+            saltedPassword.Append(BCrypt.Net.BCrypt.HashPassword(plainText));
+
+            return new string[2] { salt, saltedPassword.ToString() };
+        }
+    }
+}
