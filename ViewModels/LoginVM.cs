9993dbc20df8ac6ae230641f8f283b1e99c4c621
marthon-szabo <makotoimx@gmail.com>
2021-01-29 15:17:57 +0100

    Create login view model

---
+++ ViewModels/LoginVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlatheaGazette.ViewModels
+{
+    public class LoginVM
+    {
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public int UserId { get; set; }
+    }
+}
